af38386da9018ef2203fac76c3c0d7ed6ef3bc3d
Gaven Neufeld <gavensmac@Gavens-MacBook-Pro.local>
2018-03-26 16:26:36 -0600

    Updated CSS and HTML

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>My Madness Libs</h1>
-      </div>
+        <div>
+          <div className="backgroundSkew">
+            <div className="madlib-heading">
+              <h1>My Madness Libs</h1>
+              <div className="madlib-subheading">
+                Fill out the fields below and click the generate button<br/>to see the Madness!
+              </div>
+           </div>
+        </div>  
+      </div>   
     );
   }
 }
